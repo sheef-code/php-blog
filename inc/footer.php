f184ee1ae1774c9d36c714c467ff6519e221c63f
Shifath Salam <sheef@Shifaths-MacBook-Pro.local>
2019-02-25 23:15:52 -0800

    completed simple project

---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+<br><br><br>
+<div class="container">PHP Blog &copy
+    <?php echo date("Y"); ?>
+</div>
+<script src="http://code.jquery.com/jquery-1.12.4.js" integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU=" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+
+</html> 
